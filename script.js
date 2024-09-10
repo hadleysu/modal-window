cfd7c36f6d3419229c3cfcad7f3f415692cd9662
hadley <hadley.susu@gmail.com>
2024-09-10 13:29:59 +0800

    Add show modal button event listener

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const buttonEl = document.querySelector('.show-modal');
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+
+// Add show modal button event listener
+buttonEl.addEventListener('click', function () {
+  modalEl.classList.remove('hidden');
+  overlayEl.classList.remove('hidden');
+});
